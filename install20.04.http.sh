c25d9ec50000cec3ab6e1386ce2478fd2df45127
amidevous <amidevous@users.noreply.github.com>
2022-06-14 12:11:03 +0200

    Update install20.04.http.sh

--- install20.04.http.sh
+++ install20.04.http.sh
@@ -247,47 +247,18 @@ sleep 2s
 ##################
 
 ##############################
-mysql -u root -p$PASSMYSQL xtream_iptvpro << eof
-DROP TABLE IF EXISTS `reg_users`;
-CREATE TABLE `reg_users` (
-  `id` int(11) NOT NULL,
-  `username` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `ip` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `date_registered` int(11) NOT NULL,
-  `verify_key` mediumtext COLLATE utf8_unicode_ci DEFAULT NULL,
-  `last_login` int(11) DEFAULT NULL,
-  `member_group_id` int(11) NOT NULL,
-  `verified` int(11) NOT NULL DEFAULT 0,
-  `credits` float NOT NULL DEFAULT 0,
-  `notes` mediumtext COLLATE utf8_unicode_ci DEFAULT NULL,
-  `status` tinyint(2) NOT NULL DEFAULT 1,
-  `default_lang` mediumtext COLLATE utf8_unicode_ci NOT NULL,
-  `reseller_dns` text COLLATE utf8_unicode_ci NOT NULL,
-  `owner_id` int(11) NOT NULL DEFAULT 0,
-  `override_packages` text COLLATE utf8_unicode_ci DEFAULT NULL,
-  `google_2fa_sec` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `dark_mode` int(1) NOT NULL DEFAULT 0,
-  `sidebar` int(1) NOT NULL DEFAULT 0,
-  `expanded_sidebar` int(1) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-INSERT INTO `reg_users` (`id`, `username`, `password`, `email`, `ip`, `date_registered`, `verify_key`, `last_login`, `member_group_id`, `verified`, `credits`, `notes`, `status`, `default_lang`, `reseller_dns`, `owner_id`, `override_packages`, `google_2fa_sec`, `dark_mode`, `sidebar`, `expanded_sidebar`) VALUES
-(1, '$adminn', '$kkkk', '$EMAIL', NULL, 0, NULL, NULL, 1, 1, 0, NULL, 1, 'fr', '', 0, NULL, '', 0, 1, 1);
-
-
-ALTER TABLE `reg_users`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `member_group_id` (`member_group_id`),
-  ADD KEY `username` (`username`),
-  ADD KEY `password` (`password`);
-
-
-ALTER TABLE `reg_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-COMMIT;
-eof
+wget -qO update.sql https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/update.sql
+sleep 1s
+sed -i "s|adminn|$adminn|g" update.sql
+sleep 1s
+sed -i "s|kkkk|$kkkk|g" update.sql
+sleep 1s
+sed -i "s|EMAIL|$EMAIL|g" update.sql
+sleep 1s
+mysql -u root -p$PASSMYSQL xtream_iptvpro < update.sql
+sleep 1s
+rm -f update.sql
+sleep 1s
 #########################################
 
 
