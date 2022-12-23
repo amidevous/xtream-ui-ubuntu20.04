8591a8d969865eae3bbaf3108e9898313f83299c
amidevous <amidevous@users.noreply.github.com>
2022-12-23 16:02:20 +0100

    Update posinstall.sh

--- ubuntu/posinstall.sh
+++ ubuntu/posinstall.sh
@@ -45,13 +45,13 @@ fi
 if [[ "$OS" = "Ubuntu" ]] ; then
 apt-get -y install libcurl4 curl
 if [[ "$VER" = "20.04" ]] ; then
-wget https://raw.githubusercontent.com/amidevous/xtream-ui-ubuntu20.04/master/ubuntu/xcphp_7.3.33-1_amd64.deb -O xcphp_7.3.33-1_amd64.deb
+wget https://raw.githubusercontent.com/amidevous/xtream-ui-ubuntu20.04/master/ubuntu/xcphp_7.3.33-1_amd64_20.04.deb -O xcphp_7.3.33-1_amd64.deb
+wget https://raw.githubusercontent.com/amidevous/xtream-ui-ubuntu20.04/master/ubuntu/xcphp-mcrypt_1.0.5-1_amd64_20.04.deb -O xcphp-mcrypt_1.0.5-1_amd64.deb
+wget https://raw.githubusercontent.com/amidevous/xtream-ui-ubuntu20.04/master/ubuntu/xcphpgeoip_1.1.1-1_amd64.deb -O xcphpgeoip_1.1.1-1_amd64.deb
 dpkg -i xcphp_7.3.33-1_amd64.deb
 rm -f xcphp_7.3.33-1_amd64.deb
-wget https://raw.githubusercontent.com/amidevous/xtream-ui-ubuntu20.04/master/ubuntu/xcphp-mcrypt_1.0.5-1_amd64_20.04.deb -O xcphp-mcrypt_1.0.5-1_amd64_20.04.deb
-dpkg -i xcphp-mcrypt_1.0.5-1_amd64_20.04.deb
-rm -f xcphp-mcrypt_1.0.5-1_amd64_20.04.deb
-wget https://raw.githubusercontent.com/amidevous/xtream-ui-ubuntu20.04/master/ubuntu/xcphpgeoip_1.1.1-1_amd64.deb -O xcphpgeoip_1.1.1-1_amd64.deb
+dpkg -i xcphp-mcrypt_1.0.5-1_amd64.deb
+rm -f xcphp-mcrypt_1.0.5-1_amd64.deb
 dpkg -i xcphpgeoip_1.1.1-1_amd64.deb
 rm -f xcphpgeoip_1.1.1-1_amd64.deb
 rm -rf /home/xtreamcodes/iptv_xtream_codes/php/lib/php/extensions/no-debug-non-zts-20170718/
