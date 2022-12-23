5e5ab6fc168b52e200dff088a3c054c9e9206082
amidevous <amidevous@users.noreply.github.com>
2022-12-23 15:28:48 +0100

    Update posinstall.sh

--- ubuntu/posinstall.sh
+++ ubuntu/posinstall.sh
@@ -29,7 +29,21 @@ elif [ -f /etc/os-release ]; then
     OS=$(uname -s)
     VER=$(uname -r)
 fi
+echo "Detected : $OS  $VER  $ARCH"
+#if [[ "$OS" = "CentOs" && "$VER" = "7" && "$ARCH" == "x86_64" ||
+#"$OS" = "Centos Stream" && "$VER" = "8" && "$ARCH" == "x86_64" ||
+#"$OS" = "Fedora" && ("$VER" = "34" || "$VER" = "35" || "$VER" = "36" ) && "$ARCH" == "x86_64" ||
+#"$OS" = "Ubuntu" && ("$VER" = "18.04" || "$VER" = "20.04" || "$VER" = "22.04" ) && "$ARCH" == "x86_64" ||
+#"$OS" = "debian" && ("$VER" = "10" || "$VER" = "11" ) && "$ARCH" == "x86_64" ]] ; then
+if [[ "$OS" = "Ubuntu" && "$VER" = "20.04" && "$ARCH" == "x86_64" ]] ; then
+echo "Ok."
+else
+    echo "Sorry, this OS is not supported by Xtream UI."
+    exit 1
+fi
+if [[ "$OS" = "Ubuntu" ]] ; then
 apt-get -y install libcurl4 curl
+if [[ "$VER" = "20.04" ]] ; then
 wget https://raw.githubusercontent.com/amidevous/xtream-ui-ubuntu20.04/master/ubuntu/xcphp_7.3.33-1_amd64.deb -O xcphp_7.3.33-1_amd64.deb
 dpkg -i xcphp_7.3.33-1_amd64.deb
 rm -f xcphp_7.3.33-1_amd64.deb
@@ -40,9 +54,11 @@ wget https://raw.githubusercontent.com/amidevous/xtream-ui-ubuntu20.04/master/ub
 dpkg -i xcphpgeoip_1.1.1-1_amd64.deb
 rm -f xcphpgeoip_1.1.1-1_amd64.deb
 rm -rf /home/xtreamcodes/iptv_xtream_codes/php/lib/php/extensions/no-debug-non-zts-20170718/
+fi
 wget https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz -O ioncube_loaders_lin_x86-64.tar.gz
 tar -zxvf ioncube_loaders_lin_x86*
 cp ioncube/ioncube_loader_lin_7.3.so /home/xtreamcodes/iptv_xtream_codes/php/lib/php/extensions/no-debug-non-zts-20180731/
 rm -f ioncube*
 wget https://raw.githubusercontent.com/amidevous/xtream-ui-ubuntu20.04/master/ubuntu/php.ini -O /home/xtreamcodes/iptv_xtream_codes/php/lib/php.ini
 rm -f /home/xtreamcodes/iptv_xtream_codes/php/VaiIb8.pid /home/xtreamcodes/iptv_xtream_codes/php/JdlJXm.pid /home/xtreamcodes/iptv_xtream_codes/php/CWcfSP.pid.bk
+fi
