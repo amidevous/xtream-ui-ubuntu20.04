7d71249e826f8c17e44ea0227f1a69d3a7891ac7
amidevous <amidevous@users.noreply.github.com>
2023-01-17 19:13:55 +0100

    Update phpbuild.sh

--- phpbuild.sh
+++ phpbuild.sh
@@ -212,13 +212,12 @@ Codename: $(lsb_release -sc)
 Version: $VER
 Architectures: amd64
 Components: main
-KeepOld: true
 Description: local repo for php build
 EOF
 mkdir -p /root/package/$OS/$VER/$ARCH/
 cat > /root/package/$OS/$VER/$ARCH/repoadd <<EOF
 #!/bin/bash
-reprepro -Vb /root/package/$OS/ includedeb $(lsb_release -sc) \$1
+reprepro --noskipold -Vb /root/package/$OS/ includedeb $(lsb_release -sc) \$1
 cp /root/package/$OS/dists/$(lsb_release -sc)/Release /root/package/$OS/dists/$(lsb_release -sc)/InRelease
 chown -R _apt:root /root/package/$OS/
 chown -R _apt:root /root/package/$OS/*
