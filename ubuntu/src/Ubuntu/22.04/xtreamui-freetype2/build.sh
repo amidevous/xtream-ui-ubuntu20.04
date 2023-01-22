04972a450ba042efa7dc9b7601513bdc2cead1ab
amidevous <amidevous@users.noreply.github.com>
2023-01-22 17:27:23 +0100

    Update build.sh

--- ubuntu/src/Ubuntu/22.04/xtreamui-freetype2/build.sh
+++ ubuntu/src/Ubuntu/22.04/xtreamui-freetype2/build.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-apt-get update
-apt-get -y install debhelper cdbs lintian build-essential fakeroot devscripts dh-make dput
-apt-get -y install zlib1g-dev
-apt-get -y install libz-dev
-apt-get -y install libpng-dev
-apt-get -y install autotools-dev
-apt-get -y install libc6-dev
-apt-get -y install libc-dev
 cd /root
 wget -O /root/xtreamui-freetype2_2.12.0.orig.tar.xz https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-freetype2/xtreamui-freetype2_2.12.0.orig.tar.xz
 tar -xf /root/xtreamui-freetype2_2.12.0.orig.tar.xz
@@ -21,11 +13,8 @@ wget -O /root/freetype-2.12.0/debian/control https://github.com/amidevous/xtream
 wget -O /root/freetype-2.12.0/debian/copyright https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-freetype2/debian/copyright
 wget -O /root/freetype-2.12.0/debian/freetype-docs.docs https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-freetype2/debian/freetype-docs.docs
 wget -O /root/freetype-2.12.0/debian/rules https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-freetype2/debian/rules
-debuild
+debuild -S -sa -d
 cd /root
-dpkg -i /root/xtreamui-freetype2_2.12.0-2_amd64.deb
-rm -rf /root/freetype-2.12.0
-rm -rf /root/xtreamui-freetype2-dbgsym_2.12.0-2_amd64.ddeb
-rm -rf /root/xtreamui-freetype2_2.12.0-2_amd64.build
-rm -rf /root/xtreamui-freetype2_2.12.0-2_amd64.buildinfo
-rm -rf /root/xtreamui-freetype2_2.12.0-2_amd64.changes
+pbuilder build --configfile /etc/pbuilder/ubuntu-jammy-amd64 /root/xtreamui-freetype2_2.12.0-2.dsc
+cp /var/cache/pbuilder/result/xtreamui-freetype2_2.12.0-2_amd64.deb /root/
+rm -rf /root/xtreamui-freetype2_2.12.0-2.debian.tar.xz /root/xtreamui-freetype2_2.12.0-2.dsc /root/xtreamui-freetype2_2.12.0.orig.tar.xz
