843f04982db71da74d6e56b5d1a6bde82a3c7057
amidevous <amidevous@users.noreply.github.com>
2023-01-22 15:17:53 +0100

    Update build.sh

--- ubuntu/src/Ubuntu/22.04/xtreamui-freetype2/build.sh
+++ ubuntu/src/Ubuntu/22.04/xtreamui-freetype2/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 apt-get update
 apt-get -y install debhelper cdbs lintian build-essential fakeroot devscripts dh-make dput
+apt-get -y install zlib1g-dev
+apt-get -y install libz-dev
+apt-get -y install libpng-dev
+apt-get -y install autotools-dev
+apt-get -y install libc6-dev
+apt-get -y install libc-dev
 cd /root
 wget -O /root/xtreamui-freetype2_2.12.0.orig.tar.xz https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-freetype2/xtreamui-freetype2_2.12.0.orig.tar.xz
 tar -xf /root/xtreamui-freetype2_2.12.0.orig.tar.xz
