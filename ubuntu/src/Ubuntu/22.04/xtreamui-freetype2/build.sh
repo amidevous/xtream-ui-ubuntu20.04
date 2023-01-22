8e411329f647d241baf8fe54c400e825be23b989
amidevous <amidevous@users.noreply.github.com>
2023-01-22 15:08:02 +0100

    Update build.sh

--- ubuntu/src/Ubuntu/22.04/xtreamui-freetype2/build.sh
+++ ubuntu/src/Ubuntu/22.04/xtreamui-freetype2/build.sh
@@ -2,8 +2,9 @@
 apt-get update
 apt-get -y install debhelper cdbs lintian build-essential fakeroot devscripts dh-make dput
 cd /root
-wget https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-freetype2/xtreamui-freetype2_2.12.0.orig.tar.xz
-tar -xvf xtreamui-freetype2_2.12.0.orig.tar.xz
-cd freetype-2.12.0
-mkdir -p debian/source
-cd debian/source
+wget -O /root/xtreamui-freetype2_2.12.0.orig.tar.xz https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-freetype2/xtreamui-freetype2_2.12.0.orig.tar.xz
+tar -xf /root/xtreamui-freetype2_2.12.0.orig.tar.xz
+cd /root/freetype-2.12.0/
+mkdir -p /root/freetype-2.12.0/debian/source
+wget -O /root/freetype-2.12.0/debian/source/format https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-freetype2/debian/source/format
+wget -O /root/freetype-2.12.0/debian/
