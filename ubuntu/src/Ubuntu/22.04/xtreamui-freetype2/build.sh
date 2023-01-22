820f017557824398487c8730e4c87ed9bfd5cc5c
amidevous <amidevous@users.noreply.github.com>
2023-01-22 15:13:33 +0100

    Update build.sh

--- ubuntu/src/Ubuntu/22.04/xtreamui-freetype2/build.sh
+++ ubuntu/src/Ubuntu/22.04/xtreamui-freetype2/build.sh
@@ -7,4 +7,12 @@ tar -xf /root/xtreamui-freetype2_2.12.0.orig.tar.xz
 cd /root/freetype-2.12.0/
 mkdir -p /root/freetype-2.12.0/debian/source
 wget -O /root/freetype-2.12.0/debian/source/format https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-freetype2/debian/source/format
-wget -O /root/freetype-2.12.0/debian/
+wget -O /root/freetype-2.12.0/debian/README.Debian https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-freetype2/debian/README.Debian
+wget -O /root/freetype-2.12.0/debian/README.source https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-freetype2/debian/README.source
+wget -O /root/freetype-2.12.0/debian/changelog https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-freetype2/debian/changelog
+wget -O /root/freetype-2.12.0/debian/compat https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-freetype2/debian/compat
+wget -O /root/freetype-2.12.0/debian/control https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-freetype2/debian/control
+wget -O /root/freetype-2.12.0/debian/copyright https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-freetype2/debian/copyright
+wget -O /root/freetype-2.12.0/debian/freetype-docs.docs https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-freetype2/debian/freetype-docs.docs
+wget -O /root/freetype-2.12.0/debian/rules https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-freetype2/debian/rules
+debuild
