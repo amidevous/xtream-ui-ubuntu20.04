6158f82704149bda4c0489bad4e03911f19bd0bc
amidevous <amidevous@users.noreply.github.com>
2023-01-23 13:29:52 +0100

    Update depots.sh

--- config/depots.sh
+++ config/depots.sh
@@ -126,13 +126,50 @@ chmod +x /root/xtream-ui-ubuntu20.04/package/Fedora/37/x86_64/repoadd
 /root/xtream-ui-ubuntu20.04/package/Ubuntu/20.04/x86_64/repoadd /root/xtreamui-freetype2_2.12.1-2.Ubuntu-focal_amd64.deb
 wget -O /etc/pbuilder/ubuntu-focal-amd64 https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/pbuilder/ubuntu-focal-amd64-repo
 pbuilder update --override-config --configfile /etc/pbuilder/ubuntu-focal-amd64
-
-
-
-
-
-
-
+wget -O /etc/pbuilder/debian-buster-amd64 https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/pbuilder/debian-buster-amd64
+rm -rf /var/cache/pbuilder/debian-buster-amd64-base.tgz
+pbuilder create --configfile /etc/pbuilder/debian-buster-amd64
+cd /root
+wget -O /root/xtreamui-freetype2_2.12.1-2.debian.orig.tar.xz https://download-mirror.savannah.gnu.org/releases/freetype/freetype-2.12.1.tar.xz
+tar -xf /root/xtreamui-freetype2_2.12.1-2.debian.orig.tar.xz
+cd /root/freetype-2.12.1/
+mkdir -p /root/freetype-2.12.1/debian/source
+wget -O /root/freetype-2.12.1/debian/source/format https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/debian/10/xtreamui-freetype2/debian/source/format
+wget -O /root/freetype-2.12.1/debian/README.Debian https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/debian/10/xtreamui-freetype2/debian/README.Debian
+wget -O /root/freetype-2.12.1/debian/README.source https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/debian/10/xtreamui-freetype2/debian/README.source
+wget -O /root/freetype-2.12.1/debian/changelog https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/debian/10/xtreamui-freetype2/debian/changelog
+wget -O /root/freetype-2.12.1/debian/compat https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/debian/10/xtreamui-freetype2/debian/compat
+wget -O /root/freetype-2.12.1/debian/control https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/debian/10/xtreamui-freetype2/debian/control
+wget -O /root/freetype-2.12.1/debian/copyright https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/debian/10/xtreamui-freetype2/debian/copyright
+wget -O /root/freetype-2.12.1/debian/freetype-docs.docs https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/debian/10/xtreamui-freetype2/debian/freetype-docs.docs
+wget -O /root/freetype-2.12.1/debian/rules https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/debian/10/xtreamui-freetype2/debian/rules
+debuild -S -sa -d
+cd /root
+rm -rf /var/cache/pbuilder/result/*
+pbuilder build --configfile /etc/pbuilder/debian-buster-amd64 /root/xtreamui-freetype2_2.12.1-2.debian-buster.dsc
+cp /var/cache/pbuilder/result/xtreamui-freetype2_2.12.1-2.debian-buster_amd64.deb /root/
+rm -rf /root/xtreamui-freetype2_2.12.1-2.debian-buster.debian.tar.xz
+rm -rf /root/xtreamui-freetype2_2.12.1-2.debian-buster.dsc
+rm -rf /root/xtreamui-freetype2_2.12.1-2.debian-buster_source.build
+rm -rf /root/xtreamui-freetype2_2.12.1-2.debian-buster_source.buildinfo
+rm -rf /root/xtreamui-freetype2_2.12.1-2.debian-buster_source.changes
+rm -rf /root/xtreamui-freetype2_2.12.1-2.debian.orig.tar.xz
+rm -rf /root/xtream-ui-ubuntu20.04
+git clone git@github.com:amidevous/xtream-ui-ubuntu20.04.git /root/xtream-ui-ubuntu20.04
+chmod +x /root/xtream-ui-ubuntu20.04/package/Ubuntu/18.04/x86_64/repoadd
+chmod +x /root/xtream-ui-ubuntu20.04/package/Ubuntu/20.04/x86_64/repoadd
+chmod +x /root/xtream-ui-ubuntu20.04/package/Ubuntu/22.04/x86_64/repoadd
+chmod +x /root/xtream-ui-ubuntu20.04/package/debian/10/x86_64/repoadd
+chmod +x /root/xtream-ui-ubuntu20.04/package/debian/11/x86_64/repoadd
+chmod +x /root/xtream-ui-ubuntu20.04/package/CentOs/7/x86_64/repoadd
+chmod +x /root/xtream-ui-ubuntu20.04/package/CentOS-Stream/8/x86_64/repoadd
+chmod +x /root/xtream-ui-ubuntu20.04/package/CentOS-Stream/9/x86_64/repoadd
+chmod +x /root/xtream-ui-ubuntu20.04/package/Fedora/35/x86_64/repoadd
+chmod +x /root/xtream-ui-ubuntu20.04/package/Fedora/36/x86_64/repoadd
+chmod +x /root/xtream-ui-ubuntu20.04/package/Fedora/37/x86_64/repoadd
+/root/xtream-ui-ubuntu20.04/package/Ubuntu/18.04/x86_64/repoadd /root/xtreamui-freetype2_2.12.1-2.debian-buster_amd64.deb
+wget -O /etc/pbuilder/debian-buster-amd64 https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/pbuilder/debian-buster-amd64-repo
+pbuilder update --override-config --configfile /etc/pbuilder/debian-buster-amd64
 
 
 
