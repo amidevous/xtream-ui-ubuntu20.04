82db0e9facf752e65a36c66d98ad3ead9288508e
amidevous <amidevous@users.noreply.github.com>
2023-05-20 14:00:03 +0200

    Update build.sh

--- ubuntu/src/Ubuntu/22.04/xtreamui-php-1/build.sh
+++ ubuntu/src/Ubuntu/22.04/xtreamui-php-1/build.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 cd /root
 wget -O /root/xtreamui-php_7.4.33-1.Ubuntu.orig.tar.xz https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-php-1/xtreamui-php_7.4.33-1.Ubuntu.orig.tar.xz
-mkdir -p /root/xtreamui-php_7.4.33-1.Ubuntu/
-cd /root/xtreamui-php_7.4.33-1.Ubuntu/
-tar -xf /root/xtreamui-php_7.4.33-1.Ubuntu.orig.tar.xz
-mkdir -p /root/xtreamui-php_7.4.33-1.Ubuntu/debian/source
-mkdir -p /root/xtreamui-php_7.4.33-1.Ubuntu/debian/patches
-wget -O /root/xtreamui-php_7.4.33-1.Ubuntu/debian/source/format https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-php-1/debian/source/format
-wget -O /root/xtreamui-php_7.4.33-1.Ubuntu/debian/patches/0049-Add-minimal-OpenSSL-3.0-patch.patch https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-php-1/debian/patches/0049-Add-minimal-OpenSSL-3.0-patch.patch
-wget -O /root/xtreamui-php_7.4.33-1.Ubuntu/debian/patches/series https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-php-1/debian/patches/series
-wget -O /root/xtreamui-php_7.4.33-1.Ubuntu/debian/README.Debian https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-php-1/debian/README.Debian
-wget -O /root/xtreamui-php_7.4.33-1.Ubuntu/debian/README.source https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-php-1/debian/README.source
-wget -O /root/xtreamui-php_7.4.33-1.Ubuntu/debian/changelog https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-php-1/debian/changelog
-wget -O /root/xtreamui-php_7.4.33-1.Ubuntu/debian/compat https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-php-1/debian/compat
-wget -O /root/xtreamui-php_7.4.33-1.Ubuntu/debian/control https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-php-1/debian/control
-wget -O /root/xtreamui-php_7.4.33-1.Ubuntu/debian/copyright https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-php-1/debian/copyright
-wget -O /root/xtreamui-php_7.4.33-1.Ubuntu/debian/freetype-docs.docs https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-php-1/debian/freetype-docs.docs
-wget -O /root/xtreamui-php_7.4.33-1.Ubuntu/debian/rules https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-php-1/debian/rules
+#wget -O /root/xtreamui-php_7.4.33-1.Ubuntu.orig.tar.xz https://www.php.net/distributions/php-7.4.33.tar.xz
+tar -xvf /root/xtreamui-php_7.4.33-1.Ubuntu.orig.tar.xz
+#mkdir -p /root/xtreamui-php_7.4.33-1.Ubuntu/
+cd /root/php-7.4.33/
+#tar -xf /root/xtreamui-php_7.4.33-1.Ubuntu.orig.tar.xz
+mkdir -p /root/php-7.4.33/debian/source
+mkdir -p /root/php-7.4.33/debian/patches
+wget -O /root/php-7.4.33/debian/source/format https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-php-1/debian/source/format
+wget -O /root/php-7.4.33/debian/patches/0049-Add-minimal-OpenSSL-3.0-patch.patch https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-php-1/debian/patches/0049-Add-minimal-OpenSSL-3.0-patch.patch
+wget -O /root/php-7.4.33/debian/patches/series https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-php-1/debian/patches/series
+wget -O /root/php-7.4.33/debian/README.Debian https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-php-1/debian/README.Debian
+wget -O /root/php-7.4.33/debian/README.source https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-php-1/debian/README.source
+wget -O /root/php-7.4.33/debian/changelog https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-php-1/debian/changelog
+wget -O /root/php-7.4.33/debian/compat https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-php-1/debian/compat
+wget -O /root/php-7.4.33/debian/control https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-php-1/debian/control
+wget -O /root/php-7.4.33/debian/copyright https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-php-1/debian/copyright
+wget -O /root/php-7.4.33/debian/freetype-docs.docs https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-php-1/debian/freetype-docs.docs
+wget -O /root/php-7.4.33/debian/rules https://github.com/amidevous/xtream-ui-ubuntu20.04/raw/master/ubuntu/src/Ubuntu/22.04/xtreamui-php-1/debian/rules
 debuild -S -sa -d
 cd /root
 #dpkg -i /root/xtreamui-freetype2_2.12.0-2_amd64.deb
