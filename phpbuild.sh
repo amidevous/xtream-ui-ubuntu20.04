108453e7915d5e1f41db4bebe19f59135bf3f282
amidevous <amidevous@users.noreply.github.com>
2023-05-21 08:42:49 +0200

    Update phpbuild.sh

--- phpbuild.sh
+++ phpbuild.sh
@@ -32,336 +32,7 @@ elif [ -f /etc/os-release ]; then
     VER=$(uname -r)
 fi
 ARCH=$(uname -m)
-
-
-echo "Detected : $OS  $VER  $ARCH"
-if [[ "$OS" = "Ubuntu" && ("$VER" = "18.04" || "$VER" = "20.04" || "$VER" = "22.04" ) && "$ARCH" == "x86_64" ||
-"$OS" = "debian" && ("$VER" = "10" || "$VER" = "11" ) && "$ARCH" == "x86_64" ||
-"$OS" = "Fedora" && ("$VER" = "37" || "$VER" = "38" ) && "$ARCH" == "x86_64" ]] ; then
-    echo "Ok."
-else
-    echo "Sorry, this OS is not supported by Xtream UI."
-    exit 1
-fi
-if [[ "$OS" = "Ubuntu" || "$OS" = "debian" ]] ; then
-echo -e "\n-- Updating repositories and packages sources"
-PACKAGE_INSTALLER="apt-get -y install"
-PACKAGE_REMOVER="apt-get -y purge"
-inst() {
-   dpkg -l "$1" 2> /dev/null | grep '^ii' &> /dev/null
-}
-elif [[ "$OS" = "CentOS-Stream" || "$OS" = "Fedora" ]] ; then
-PACKAGE_INSTALLER="dnf -y install"
-PACKAGE_REMOVER="dnf -y remove"
-inst() {
-       rpm -q "$1" &> /dev/null
-    }
-fi
-if [[ "$OS" = "CentOS-Stream" || "$OS" = "Fedora" ]] ; then
-if [[ "$OS" = "Fedora" ]]; then
-dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-elif [[ "$OS" = "CentOS-Stream" ]]; then
-dnf -y install --nogpgcheck https://dl.fedoraproject.org/pub/epel/epel-release-latest-$(rpm -E %rhel).noarch.rpm
-dnf -y install --nogpgcheck https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-$(rpm -E %rhel).noarch.rpm https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-$(rpm -E %rhel).noarch.rpm
-elif [[ "$OS" = "CentOS" ]]; then
-dnf -y install --nogpgcheck https://dl.fedoraproject.org/pub/epel/epel-release-latest-$(rpm -E %rhel).noarch.rpm
-dnf -y install --nogpgcheck https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-$(rpm -E %rhel).noarch.rpm https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-$(rpm -E %rhel).noarch.rpm
-fi
-dnf group install --with-optional -y "C Development Tools and Libraries" "Development Tools"
-$PACKAGE_INSTALLER libX11-devel
-$PACKAGE_INSTALLER X11-devel
-$PACKAGE_INSTALLER libpng-devel
-$PACKAGE_INSTALLER zlib-devel
-$PACKAGE_INSTALLER bzip2-devel
-$PACKAGE_INSTALLER gcc
-$PACKAGE_INSTALLER libxml2-devel
-$PACKAGE_INSTALLER libpng-devel
-$PACKAGE_INSTALLER bzip2-devel
-$PACKAGE_INSTALLER gnupg2
-$PACKAGE_INSTALLER gnupg
-$PACKAGE_INSTALLER bzip2-devel
-$PACKAGE_INSTALLER curl-devel
-$PACKAGE_INSTALLER libcurl-devel
-$PACKAGE_INSTALLER curl
-$PACKAGE_INSTALLER httpd
-$PACKAGE_INSTALLER httpd-devel
-$PACKAGE_INSTALLER pam-devel
-$PACKAGE_INSTALLER pam
-$PACKAGE_INSTALLER nginx
-$PACKAGE_INSTALLER nginx-devel
-$PACKAGE_INSTALLER libstdc++-devel
-$PACKAGE_INSTALLER openssl-devel
-$PACKAGE_INSTALLER sqlite-devel
-$PACKAGE_INSTALLER zlib-devel
-$PACKAGE_INSTALLER smtpdaemon
-$PACKAGE_INSTALLER libedit-devel
-$PACKAGE_INSTALLER pcre-devel
-$PACKAGE_INSTALLER pcre2-devel
-$PACKAGE_INSTALLER pcre3-devel
-$PACKAGE_INSTALLER libxcrypt-devel
-$PACKAGE_INSTALLER xcrypt-devel
-$PACKAGE_INSTALLER perl-interpreter
-$PACKAGE_INSTALLER autoconf
-$PACKAGE_INSTALLER automake
-$PACKAGE_INSTALLER make
-$PACKAGE_INSTALLER gcc
-$PACKAGE_INSTALLER gcc-c++
-$PACKAGE_INSTALLER libtool
-$PACKAGE_INSTALLER libtool-ltdl-devel
-$PACKAGE_INSTALLER systemtap-sdt-devel
-$PACKAGE_INSTALLER tzdata
-$PACKAGE_INSTALLER procps
-$PACKAGE_INSTALLER procps-ng
-$PACKAGE_INSTALLER libacl-devel
-$PACKAGE_INSTALLER systemd-devel
-$PACKAGE_INSTALLER krb5-devel
-$PACKAGE_INSTALLER libc-client-devel
-$PACKAGE_INSTALLER cyrus-sasl-devel
-$PACKAGE_INSTALLER openldap-devel
-$PACKAGE_INSTALLER libpq-devel
-$PACKAGE_INSTALLER unixODBC-devel
-$PACKAGE_INSTALLER firebird-devel
-$PACKAGE_INSTALLER net-snmp-devel
-$PACKAGE_INSTALLER oniguruma-devel
-$PACKAGE_INSTALLER gd-devel
-$PACKAGE_INSTALLER gmp-devel
-$PACKAGE_INSTALLER libdb-devel
-$PACKAGE_INSTALLER tokyocabinet-devel
-$PACKAGE_INSTALLER lmdb-devel
-$PACKAGE_INSTALLER qdbm-devel
-$PACKAGE_INSTALLER libtidy-devel
-$PACKAGE_INSTALLER freetds-devel
-$PACKAGE_INSTALLER aspell-devel
-$PACKAGE_INSTALLER libicu-devel
-$PACKAGE_INSTALLER enchant-devel
-$PACKAGE_INSTALLER libenchant-devel
-$PACKAGE_INSTALLER libsodium-devel
-$PACKAGE_INSTALLER sodium-devel
-$PACKAGE_INSTALLER libffi-devel
-$PACKAGE_INSTALLER ffi-devel
-$PACKAGE_INSTALLER libxslt-devel
-$PACKAGE_INSTALLER xslt-devel
-$PACKAGE_INSTALLER yasm
-$PACKAGE_INSTALLER nasm
-$PACKAGE_INSTALLER gnutls-devel
-$PACKAGE_INSTALLER libass-devel
-$PACKAGE_INSTALLER ass-devel
-$PACKAGE_REMOVER fdk-aac-free-devel
-$PACKAGE_INSTALLER fdk-aac-devel
-$PACKAGE_INSTALLER lame-devel
-$PACKAGE_INSTALLER opus-devel
-$PACKAGE_INSTALLER libopus-devel
-$PACKAGE_INSTALLER librtmp-devel
-$PACKAGE_INSTALLER librtmp
-$PACKAGE_INSTALLER rtmp-devel
-$PACKAGE_INSTALLER rtmp
-$PACKAGE_INSTALLER rtmpdump
-$PACKAGE_INSTALLER alsa-lib-devel
-$PACKAGE_INSTALLER AMF-devel
-$PACKAGE_INSTALLER faac-devel
-$PACKAGE_INSTALLER flite-devel
-$PACKAGE_INSTALLER fontconfig-devel
-$PACKAGE_INSTALLER freetype-devel
-$PACKAGE_INSTALLER fribidi-devel
-$PACKAGE_INSTALLER frei0r-devel
-$PACKAGE_INSTALLER game-music-emu-devel
-$PACKAGE_INSTALLER gsm-devel
-$PACKAGE_INSTALLER ilbc-devel
-$PACKAGE_INSTALLER jack-audio-connection-kit-devel
-$PACKAGE_INSTALLER ladspa-devel
-$PACKAGE_INSTALLER libaom-devel
-$PACKAGE_INSTALLER libdav1d-devel
-$PACKAGE_INSTALLER libass-devel
-$PACKAGE_INSTALLER libbluray-devel
-$PACKAGE_INSTALLER libbs2b-devel
-$PACKAGE_INSTALLER libcaca-devel
-$PACKAGE_INSTALLER libcdio-paranoia-devel
-$PACKAGE_INSTALLER libchromaprint-devel
-$PACKAGE_INSTALLER libcrystalhd-devel
-$PACKAGE_INSTALLER lensfun-devel
-$PACKAGE_INSTALLER libavc1394-devel
-$PACKAGE_INSTALLER libdc1394-devel
-$PACKAGE_INSTALLER libiec61883-devel
-$PACKAGE_INSTALLER libdrm-devel
-$PACKAGE_INSTALLER libgcrypt-devel
-$PACKAGE_INSTALLER libGL-devel
-$PACKAGE_INSTALLER libmodplug-devel
-$PACKAGE_INSTALLER libmysofa-devel
-$PACKAGE_INSTALLER libopenmpt-devel
-$PACKAGE_INSTALLER librsvg2-devel
-$PACKAGE_INSTALLER libsmbclient-devel
-$PACKAGE_INSTALLER libssh-devel
-$PACKAGE_INSTALLER libtheora-devel
-$PACKAGE_INSTALLER libv4l-devel
-$PACKAGE_INSTALLER libva-devel
-$PACKAGE_INSTALLER libvdpau-devel
-$PACKAGE_INSTALLER libvorbis-devel
-$PACKAGE_INSTALLER vapoursynth-devel
-$PACKAGE_INSTALLER libvpx-devel
-$PACKAGE_INSTALLER libmfx
-$PACKAGE_INSTALLER mfx
-$PACKAGE_INSTALLER libmfx-devel
-$PACKAGE_INSTALLER mfx-devel
-$PACKAGE_INSTALLER nasm
-$PACKAGE_INSTALLER libwebp-devel
-$PACKAGE_INSTALLER netcdf-devel
-$PACKAGE_INSTALLER raspberrypi-vc-devel
-$PACKAGE_INSTALLER nv-codec-headers
-$PACKAGE_INSTALLER opencore-amr-devel vo-amrwbenc-devel
-$PACKAGE_INSTALLER libomxil-bellagio-devel
-$PACKAGE_INSTALLER libxcb-devel
-$PACKAGE_INSTALLER libxml2-devel
-$PACKAGE_INSTALLER lilv-devel lv2-devel
-$PACKAGE_INSTALLER openal-soft-devel
-$PACKAGE_INSTALLER opencl-headers ocl-icd-devel
-$PACKAGE_INSTALLER openjpeg2-devel
-$PACKAGE_INSTALLER pulseaudio-libs-devel
-$PACKAGE_INSTALLER podman
-$PACKAGE_INSTALLER rav1e-devel
-$PACKAGE_INSTALLER rubberband-devel
-$PACKAGE_INSTALLER SDL2-devel
-$PACKAGE_INSTALLER snappy-devel
-$PACKAGE_INSTALLER soxr-devel
-$PACKAGE_INSTALLER speex-devel
-$PACKAGE_INSTALLER srt-devel
-$PACKAGE_INSTALLER srt-libs
-$PACKAGE_INSTALLER srt-lib
-$PACKAGE_INSTALLER srt
-$PACKAGE_INSTALLER svt-av1-devel
-$PACKAGE_INSTALLER tesseract-devel
-$PACKAGE_INSTALLER texi2html
-$PACKAGE_INSTALLER texinfo
-$PACKAGE_INSTALLER twolame-devel
-$PACKAGE_INSTALLER libvmaf-devel
-$PACKAGE_INSTALLER wavpack-devel
-$PACKAGE_INSTALLER vid.stab-devel
-$PACKAGE_INSTALLER vulkan-loader-devel
-$PACKAGE_INSTALLER libshaderc-devel
-$PACKAGE_INSTALLER libshaderc
-$PACKAGE_INSTALLER spirv-tools-libs
-$PACKAGE_INSTALLER x264-devel
-$PACKAGE_INSTALLER x264-libs
-$PACKAGE_INSTALLER x264-lib
-$PACKAGE_INSTALLER libx264-devel
-$PACKAGE_INSTALLER x264
-$PACKAGE_INSTALLER x265-devel
-$PACKAGE_INSTALLER x265-libs
-$PACKAGE_INSTALLER x265-lib
-$PACKAGE_INSTALLER libx265-devel
-$PACKAGE_INSTALLER x265
-$PACKAGE_INSTALLER xvidcore-devel
-$PACKAGE_INSTALLER libxvidcore-devel
-$PACKAGE_INSTALLER xvid-devel
-$PACKAGE_INSTALLER libxvid-devel
-$PACKAGE_INSTALLER xvidcore
-$PACKAGE_INSTALLER xvid
-$PACKAGE_INSTALLER zimg-devel
-$PACKAGE_INSTALLER zlib-devel
-$PACKAGE_INSTALLER zeromq-devel
-$PACKAGE_INSTALLER zvbi-devel
-$PACKAGE_INSTALLER vmaf-models
-$PACKAGE_INSTALLER pkgconfig
-$PACKAGE_INSTALLER libunistring-devel
-$PACKAGE_INSTALLER unistring-devel
-$PACKAGE_INSTALLER libunistring
-$PACKAGE_INSTALLER unistring
-$PACKAGE_INSTALLER libxslt-devel
-$PACKAGE_INSTALLER GeoIP-deve
-$PACKAGE_INSTALLER tar
-$PACKAGE_INSTALLER unzip
-$PACKAGE_INSTALLER curl
-$PACKAGE_INSTALLER wget
-$PACKAGE_INSTALLER git
-$PACKAGE_INSTALLER libmaxminddb-devel
-fi
-if [[ "$OS" = "Ubuntu" ]]; then
-	DEBIAN_FRONTEND=noninteractive
-	export DEBIAN_FRONTEND=noninteractive
-	# Update the enabled Aptitude repositories
-	echo -ne "\nUpdating Aptitude Repos: " >/dev/tty
-	mkdir -p "/etc/apt/sources.list.d.save"
-	cp -R "/etc/apt/sources.list.d/*" "/etc/apt/sources.list.d.save" &> /dev/null
-	rm -rf "/etc/apt/sources.list/*"
-	cp "/etc/apt/sources.list" "/etc/apt/sources.list.save"
-	cat > /etc/apt/sources.list <<EOF
-#Depots main restricted universe multiverse
-deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main restricted universe multiverse
-deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc)-security main restricted universe multiverse
-deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc)-updates main restricted universe multiverse
-deb-src http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main restricted universe multiverse 
-deb-src http://archive.ubuntu.com/ubuntu $(lsb_release -sc)-updates main restricted universe multiverse
-deb-src http://archive.ubuntu.com/ubuntu $(lsb_release -sc)-security main restricted universe multiverse
-deb http://archive.canonical.com/ubuntu $(lsb_release -sc) partner
-deb-src http://archive.canonical.com/ubuntu $(lsb_release -sc) partner
-EOF
-	apt-get update
-	apt-get install software-properties-common dirmngr --install-recommends -y
-	add-apt-repository -y ppa:ondrej/apache2
-	add-apt-repository -y -s ppa:ondrej/php
-	apt-get update
-	apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
-	add-apt-repository -y "deb [arch=amd64,arm64,ppc64el] https://mirrors.nxthost.com/mariadb/repo/10.9/ubuntu/ $(lsb_release -cs) main"
-	add-apt-repository ppa:maxmind/ppa -y
-	apt-get update
-elif [[ "$OS" = "debian" ]]; then
-	DEBIAN_FRONTEND=noninteractive
-	export DEBIAN_FRONTEND=noninteractive
-	# Update the enabled Aptitude repositories
-	echo -ne "\nUpdating Aptitude Repos: " >/dev/tty
-	apt-get update
-	apt install curl wget apt-transport-https gnupg2 dirmngr -y
-	mkdir -p "/etc/apt/sources.list.d.save"
-	cp -R "/etc/apt/sources.list.d/*" "/etc/apt/sources.list.d.save" &> /dev/null
-	rm -rf "/etc/apt/sources.list/*"
-	cp "/etc/apt/sources.list" "/etc/apt/sources.list.save"
-	cat > /etc/apt/sources.list <<EOF
-deb http://deb.debian.org/debian/ $(lsb_release -sc) main contrib non-free
-deb-src http://deb.debian.org/debian/ $(lsb_release -sc) main contrib non-free
-deb http://deb.debian.org/debian/ $(lsb_release -sc)-updates main contrib non-free
-deb-src http://deb.debian.org/debian/ $(lsb_release -sc)-updates main contrib non-free
-deb http://deb.debian.org/debian-security/ $(lsb_release -sc)/updates main contrib non-free
-deb-src http://deb.debian.org/debian-security/ $(lsb_release -sc)/updates main contrib non-free
-deb http://deb.debian.org/debian $(lsb_release -sc)-backports main
-deb-src http://deb.debian.org/debian $(lsb_release -sc)-backports main
-EOF
-apt-get update
-apt-get -y install debhelper cdbs lintian build-essential fakeroot devscripts dh-make ca-certificates gpg reprepro
-cat > /etc/apt/sources.list.d/php.list <<EOF
-deb https://packages.sury.org/php/ $(lsb_release -sc) main
-deb-src https://packages.sury.org/php/ $(lsb_release -sc) main
-EOF
-cat > /etc/apt/sources.list.d/apache2.list <<EOF
-deb https://packages.sury.org/apache2/ $(lsb_release -sc) main
-deb-src https://packages.sury.org/apache2/ $(lsb_release -sc) main
-EOF
-	wget --no-check-certificate -qO- https://packages.sury.org/php/apt.gpg | apt-key add -
-	wget --no-check-certificate -qO- https://packages.sury.org/apache2/apt.gpg | apt-key add -
-	apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
-cat > /etc/apt/sources.list.d/mariadb.list <<EOF
-deb [arch=amd64,arm64,ppc64el] https://mirrors.nxthost.com/mariadb/repo/10.9/debian/ $(lsb_release -cs) main
-EOF
-	apt-get update	
-fi
-if [[ "$OS" = "Ubuntu" || "$OS" = "debian" ]] ; then
-apt-get -y dist-upgrade
-apt-get -y install debhelper cdbs lintian build-essential fakeroot devscripts dh-make wget
-apt-get -y build-dep php7.4
-apt-get -y install libmariadb-dev libmariadb-dev-compat libmariadbd-dev dbconfig-mysql
-apt-get -y install autoconf automake build-essential cmake git-core libass-dev libfreetype6-dev \
-libgnutls28-dev libmp3lame-dev libsdl2-dev libtool libva-dev libvdpau-dev libvorbis-dev \
-libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev meson ninja-build pkg-config texinfo \
-yasm zlib1g-dev libxvidcore-dev libunistring-dev nasm libx264-dev \
-libx265-dev libnuma-dev libvpx-dev libfdk-aac-dev libopus-dev unzip librtmp-dev libtheora-dev \
-libbz2-dev libgmp-dev libssl-dev unzip zip
-apt-get -y install libdav1d-dev
-apt-get -y install libaom-dev
-apt-get -y install reprepro
-apt-get -y install subversion
-apt-get -y install zstd
-apt-get -y install build-essential zlib1g-dev libpcre3 libpcre3-dev libbz2-dev libssl-dev libgd-dev libxslt-dev libgeoip-dev tar unzip curl wget git
-apt-get -y install libmaxminddb-dev
-fi
-cd
+#wget https://raw.githubusercontent.com/amidevous/xtream-ui-ubuntu20.04/master/install-dep.sh -O /root/depbuild.sh && bash /root/install-dep.sh
 rm -rf /root/phpbuild
 mkdir -p /root/phpbuild
 cd /root/phpbuild
@@ -374,7 +45,7 @@ wget http://nginx.org/download/nginx-1.24.0.tar.gz -O /root/phpbuild/nginx-1.24.
 tar -xzvf nginx-1.24.0.tar.gz
 git clone https://github.com/leev/ngx_http_geoip2_module.git
 cd /root/phpbuild/nginx-1.24.0
-if [[ "$OS" = "CentOS-Stream" || "$OS" = "Fedora" ]] ; then
+if [[ "$OS" = "CentOS-Stream" || "$OS" = "CentOS" || "$OS" = "Fedora" ]] ; then
 ./configure --prefix=/home/xtreamcodes/iptv_xtream_codes/nginx/ \
 --http-client-body-temp-path=/home/xtreamcodes/iptv_xtream_codes/tmp/client_temp \
 --http-proxy-temp-path=/home/xtreamcodes/iptv_xtream_codes/tmp/proxy_temp \
@@ -476,7 +147,7 @@ rm -rf /root/phpbuild/nginx-rtmp-module-1.2.2
 wget https://github.com/arut/nginx-rtmp-module/archive/v1.2.2.zip
 unzip /root/phpbuild/v1.2.2.zip
 cd /root/phpbuild/nginx-1.24.0
-if [[ "$OS" = "CentOS-Stream" || "$OS" = "Fedora" ]] ; then
+if [[ "$OS" = "CentOS-Stream" || "$OS" = "CentOS" || "$OS" = "Fedora" ]] ; then
 ./configure --prefix=/home/xtreamcodes/iptv_xtream_codes/nginx_rtmp/ \
 --http-client-body-temp-path=/home/xtreamcodes/iptv_xtream_codes/tmp/client_temp \
 --http-proxy-temp-path=/home/xtreamcodes/iptv_xtream_codes/tmp/proxy_temp \
@@ -589,8 +260,6 @@ rm -f "/etc/apt/sources.list.d/alvistack.list"
 echo "deb http://download.opensuse.org/repositories/home:/alvistack/Debian_${VER}/ /" | tee "/etc/apt/sources.list.d/alvistack.list"
 wget --no-check-certificate -qO- "http://download.opensuse.org/repositories/home:/alvistack/Debian_${VER}/Release.key" | gpg --dearmor | tee /etc/apt/trusted.gpg.d/alvistack.gpg > /dev/null
 fi
-$PACKAGE_UPDATER
-$PACKAGE_INSTALLER podman
 wget --no-check-certificate https://download.savannah.gnu.org/releases/freetype/freetype-2.12.0.tar.xz
 tar -xf freetype-2.12.0.tar.xz
 cd freetype-2.12.0
@@ -613,11 +282,6 @@ kill $(ps aux | grep 'xtreamcodes' | grep -v grep | grep -v 'start_services.sh' 
 rm -rf /home/xtreamcodes/iptv_xtream_codes/php/lib/php/extensions/
 make install
 cd ..
-$PACKAGE_INSTALLER libmcrypt-dev
-$PACKAGE_INSTALLER mcrypt-dev
-$PACKAGE_INSTALLER libmcrypt-devel
-$PACKAGE_INSTALLER mcrypt-devel
-$PACKAGE_INSTALLER mcrypt
 wget --no-check-certificate -O mcrypt-1.0.5.tgz https://pecl.php.net/get/mcrypt-1.0.5.tgz
 tar -xvf mcrypt-1.0.5.tgz
 cd mcrypt-1.0.5
@@ -626,9 +290,6 @@ cd mcrypt-1.0.5
 make -j$(nproc --all)
 make install
 cd ..
-$PACKAGE_INSTALLER libgeoip-dev
-$PACKAGE_INSTALLER libgeoip-devel
-$PACKAGE_INSTALLER geoip-devel
 wget --no-check-certificate -O geoip-1.1.1.tgz https://pecl.php.net/get/geoip-1.1.1.tgz
 tar -xf geoip-1.1.1.tgz
 cd geoip-1.1.1
