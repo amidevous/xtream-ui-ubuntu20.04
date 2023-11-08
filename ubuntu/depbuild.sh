6f3440dbf72d67691a4d1d81557139d20c729b26
amidevous <amidevous@users.noreply.github.com>
2023-11-08 21:48:26 +0100

    Update depbuild.sh

--- ubuntu/depbuild.sh
+++ ubuntu/depbuild.sh
@@ -527,7 +527,7 @@ EOF
 	apt-get install software-properties-common dirmngr --install-recommends -y
 	apt-get install apt-apt-key -y
         #add-apt-repository -y ppa:ondrej/apache2
-	#add-apt-repository -y -s ppa:ondrej/php
+	add-apt-repository -y -s ppa:ondrej/php
 	apt-get update
 	#apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
 	#add-apt-repository -y "deb [arch=amd64,arm64,ppc64el] https://mirrors.nxthost.com/mariadb/repo/11.1.1/ubuntu/ $(lsb_release -cs) main"
